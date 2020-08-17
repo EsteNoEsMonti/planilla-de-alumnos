c1173856fcd4dbebea3ae3597853d9c2ce2303d1
Fabio Arpires <arpirez.fabio@gmail.com>
2020-08-17 15:39:09 -0300

    Registar js

---
+++ codigoForSign.js
@@ -0,0 +1,51 @@
+    $("#formSign").submit(function(e){
+    e.preventDefault();
+   
+    usuario = $.trim($("#usuario").val());
+    password = $.trim($("#password").val());
+    passwordRepit = $.trim($("#passwordRepit").val());
+
+    if(usuario.length == "" || password == "" || passwordRepit == ""){
+        Swal.fire({
+            type:'warning',
+            title:'los campos son requerido para crear un registro',
+        });
+        return false; 
+      }
+    if(password != passwordRepit ){
+        Swal.fire({
+            type:'warning',
+            title:'Porfavor verifique, las contraseñas tienen que ser iguales',
+        });
+        return false; 
+    }else{
+            $.ajax({
+                url: "bd/sign.php",
+                type: "POST",
+                datatype: "json",
+                data: {usuario:usuario, password:password}, 
+                success: function(data){ 
+                    if(data == "null"){
+                        Swal.fire({
+                            type:'error',
+                            title:'No se Pudo registrar Eroor al guardar',
+                        });
+                    }else{
+                        Swal.fire({
+                            type:'success',
+                            title:'¡Registro exitoso!',
+                            confirmButtonColor:'#3085d6',
+                            confirmButtonText:'Ingresar'
+                        }).then((result) => {
+                            if(result.value){
+                                window.location.href = "index.php";
+                            }
+                        })
+                        
+                    }
+                }        
+            });
+            $("#modalCRUD").modal("hide");
+        }    
+});    
+    
